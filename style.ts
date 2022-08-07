dc6751739008f4f19a02117e6097e0413bb15bc2
alan <alan.fernandes63@gmail.com>
2022-08-07 14:02:06 -0300

    Adiciona Styled Components
    Adiciona Styled Components

---
+++ style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  display: flex;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+`;
+export default Container;
